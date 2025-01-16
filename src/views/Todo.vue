521af83ba07df846c3ff7dc18f0eac4eb44d87d3
Afzaal565 <afzaalhussain565@gmail.com>
2025-01-16 08:43:08 +0500

    todo crud add using pinia store and composable usefetch for apis

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,7 +9,7 @@ const taskForm = toRef(taskStore, 'taskForm');
 const task = toRef(taskStore, 'task');
 
 const resetRef = ref();
-
+const isEdit = ref<boolean>(false);
 
 onMounted(async () => {
     await taskStore.getTasks();
@@ -28,25 +28,63 @@ const saveTask = async () => {
     }
 }
 
-const removeTask = (index: number) => {
-    tasks.value.splice(index, 1)
+const editTask = (item: Task) => {
+    task.value = item;
+    isEdit.value = true;
+    taskForm.value.task_name = item.task_name;
+    taskForm.value.description = item.description;
 }
+
+const removeTask = async (item?: Task) => {
+    if (item) {
+        await taskStore.deleteTask(item.id);
+        taskList.value = taskList.value.filter((task) => task.id !== item.id);
+    }
+}
+
+
+const updateTask = async () => {
+    if (taskForm.value.task_name !== null) {
+        await taskStore.updateTask(task.value?.id);
+        await taskStore.getTasks();
+        window.setTimeout(() => {
+            resetRef.value.click();
+        }, 500);
+    }
+}
+
+const reset = async () => {
+    taskForm.value.task_name = null;
+    taskForm.value.description = null;
+    task.value = null;
+    isEdit.value = false;
+}
+
+
 </script>
 <template>
     <div class="container">
-        <form @submit.prevent="saveTask">
-            <div class="mb-3">
-                <label for="task-name" class="form-label">Task Name</label>
-                <input type="text" v-model="taskForm.task_name" class="form-control" id="task-name"
-                    placeholder="enter task name">
-            </div>
-            <div class="mb-3">
-                <label for="task-desc" class="form-label">Description</label>
-                <textarea class="form-control" v-model="taskForm.description" id="task-desc" rows="3"></textarea>
-            </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">Save Changes</button>
-                <button type="reset" ref="resetRef" class="btn btn-primary">Cancel</button>
+        <form>
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <label for="task-name" class="form-label">Task Name</label>
+                    <input type="text" v-model="taskForm.task_name" class="form-control" id="task-name"
+                        placeholder="enter task name">
+                </div>
+                <div class="col-12 mb-3">
+                    <label for="task-desc" class="form-label">Description</label>
+                    <textarea class="form-control" v-model="taskForm.description" id="task-desc" rows="3"></textarea>
+                </div>
+                <div class="col-12 mb-3">
+                    <button v-if="!isEdit" type="button" class="btn btn-primary float-end"
+                        :disabled="taskForm.task_name == ''" @click="saveTask">Save
+                        Changes</button>
+                    <button v-else type="button" class="btn btn-primary float-end" :disabled="taskForm.task_name == ''"
+                        @click="updateTask">Update
+                        Changes</button>
+                    <button type="reset" ref="resetRef" class="btn btn-danger float-end me-2"
+                        @click="reset">Cancel</button>
+                </div>
             </div>
         </form>
 
@@ -68,8 +106,8 @@ const removeTask = (index: number) => {
                         <td>{{ item?.task_name }}</td>
                         <td>{{ item?.description }}</td>
                         <td>
-                            <button class="btn btn-primary">Edit</button>
-                            <button class="btn btn-danger">Delete</button>
+                            <button class="btn btn-primary me-2 " @click="editTask(item)">Edit</button>
+                            <button class="btn btn-danger" @click="removeTask(item)">Delete</button>
                         </td>
                     </tr>
                 </tbody>
